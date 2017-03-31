025b9c00d2e1c3e9bcd6e260e3c820f7edec6f9e
Of3zKpTwj0 <Of3zKpTwj0@grr.la>
2017-03-31 19:13:50 -0400

    Initial commit

---
+++ Test Desktop Client/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Test Desktop Client")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Of3zKpTwj0")>
+<Assembly: AssemblyProduct("Of3zKpTwj0")>
+<Assembly: AssemblyCopyright("Copyright © Of3zKpTwj0 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("309e0c73-171e-4a35-b3ae-da3e7d505f48")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
