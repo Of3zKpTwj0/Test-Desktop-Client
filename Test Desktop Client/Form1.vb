44bae3c3a07dc3e603cfdd1d5cc0ce5275436235
Of3zKpTwj0 <Of3zKpTwj0@grr.la>
2017-03-31 19:46:00 -0400

    Test diff

--- Test Desktop Client/Form1.vb
+++ Test Desktop Client/Form1.vb
@@ -2,6 +2,11 @@
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 		Debug.WriteLine("Hello world!")
+#If DEBUG Then
+        Debug.WriteLine("Hello world!")
+#ElseIf RELEASE Then
+        Console.WriteLine("Hello world!")     
+#End If
     End Sub
 
 End Class
